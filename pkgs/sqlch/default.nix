a38125478289becf3cb3e73e2d7a03029f0b77a4
Philip Repko <philip.repko@gmail.com>
2026-01-30 13:26:55 -0500

    adding sqlch.tui

--- pkgs/sqlch/default.nix
+++ pkgs/sqlch/default.nix
@@ -1,33 +1,24 @@
-{ lib, python3Packages, fetchFromGitHub }:
-
+{ lib, python3Packages }:
 python3Packages.buildPythonApplication {
   pname = "sqlch";
   version = "0.1.0";
-
-  src = fetchFromGitHub {
-    owner = "SW-philip";
-    repo = "sqlch";
-    rev = "main";
-    sha256 = "sha256-REPLACE_ME";
-  };
-
+  src = ../../.;
+  
   pyproject = true;
-
+  
   nativeBuildInputs = [
     python3Packages.setuptools
     python3Packages.wheel
   ];
-
+  
   propagatedBuildInputs = [
     python3Packages.requests
     python3Packages.textual
   ];
-
-  # sanity check: fails early if imports are broken
+  
   pythonImportsCheck = [ "sqlch" ];
-
   doCheck = false;
-
+  
   meta = with lib; {
     description = "Headless radio + TUI streaming controller";
     license = licenses.mit;
