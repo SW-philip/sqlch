b46b23de2c2c761d11b0eb7d8f8fdc74b5fa8518
Philip Repko <philip.repko@gmail.com>
2026-02-17 11:09:06 -0500

    resolve merge conflict in default.nix

--- pkgs/sqlch/default.nix
+++ pkgs/sqlch/default.nix
@@ -1,52 +1,33 @@
 { lib, pkgs, python3Packages, fetchFromGitHub }:
-
-python3Packages.buildPythonApplication {
-  pname = "sqlch";
-  version = "0.1.1";
-
-  src = fetchFromGitHub {
-    owner = "SW-philip";
-    repo  = "sqlch";
-    sha256 = lib.fakeSha256;
-  };
-
-  pyproject = true;
-
-  nativeBuildInputs = with python3Packages; [
-    setuptools
-    wheel
-  ];
-
-  propagatedBuildInputs = with python3Packages; [
-    requests
-    textual
-  ];
-
-  # Tools sqlch uses at runtime
+let
   runtimeDeps = [
     pkgs.mpv
     pkgs.socat
     pkgs.procps
     pkgs.mpvScripts.mpris
   ];
-
+in
+python3Packages.buildPythonApplication {
+  pname = "sqlch";
+  version = "0.1.1";
+  src = fetchFromGitHub {
+    owner = "SW-philip";
+    repo  = "sqlch";
+    rev   = "main";
+    sha256 = "sha256-WHdASmHSWZDOI6+gIuyjlMa/b3DZEqk3fgpJlz+7Vkw=";
+  };
+  pyproject = true;
+  nativeBuildInputs = with python3Packages; [ setuptools wheel ];
+  propagatedBuildInputs = with python3Packages; [ requests textual ];
   buildInputs = runtimeDeps;
-
   postFixup = ''
     wrapProgram $out/bin/sqlch \
       --prefix PATH : ${lib.makeBinPath runtimeDeps} \
       --set MPV_BIN ${pkgs.mpv}/bin/mpv \
       --set SQLCH_MPRIS_PLUGIN ${pkgs.mpvScripts.mpris}/share/mpv/scripts/mpris.so
   '';
-
-  pythonImportsCheck = [
-    "sqlch"
-    "sqlch.cli.main"
-    "sqlch.tui.app"
-  ];
-
+  pythonImportsCheck = [ "sqlch" "sqlch.cli.main" "sqlch.tui.app" ];
   doCheck = false;
-
   meta = with lib; {
     description = "Headless radio + TUI streaming controller";
     license = licenses.mit;
