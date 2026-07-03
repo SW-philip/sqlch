e7d2195bd586a353bf8d8c80534f1a6efca9b07f
Phil Repko <philipjrepko@gmail.com>
2026-07-03 13:13:16 -0400

    flip album art

--- sqlch_gui/__main__.py
+++ sqlch_gui/__main__.py
@@ -9,7 +9,7 @@ from .ui.window import SqlchPopupWindow
 
 class SqlchGuiApp(Gtk.Application):
     def __init__(self):
-        super().__init__(application_id="dev.prepko.sqlch-gui")
+        super().__init__(application_id="dev.prepko.sqlch_gui")
 
     def do_activate(self):
         win = SqlchPopupWindow(self)
@@ -18,7 +18,7 @@ class SqlchGuiApp(Gtk.Application):
 def main():
     # Sync and guarantee frequency mapping configuration arrays are healthy
     library.backfill_freqs()
-    
+
     app = SqlchGuiApp()
     sys.exit(app.run(sys.argv))
 
